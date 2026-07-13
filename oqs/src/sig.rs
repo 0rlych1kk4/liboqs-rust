d5bbf1ea6c68dd681fc2292eee801f2cc4930f2f
0rlych1kk4 <orlychikka@gmail.com>
2026-07-13 22:21:39 +0800

    fix: address Clippy warnings in RustCrypto adapters
    Signed-off-by: 0rlych1kk4 <orlychikka@gmail.com>

--- oqs/src/sig.rs
+++ oqs/src/sig.rs
@@ -585,10 +585,7 @@ pub struct Signer<'a> {
 impl<'a> Signer<'a> {
     /// Creates a new [`Signer`] with the given signature scheme and secret key.
     pub fn new(scheme: &'a Sig, sk: SecretKey) -> Self {
-        Signer {
-            scheme: scheme,
-            sk: sk,
-        }
+        Signer { scheme, sk }
     }
 }
 
@@ -627,10 +624,7 @@ pub struct Verifier<'a> {
 impl<'a> Verifier<'a> {
     /// Creates a new [`Verifier`] with the given signature scheme and public key.
     pub fn new(scheme: &'a Sig, pk: PublicKey) -> Self {
-        Verifier {
-            scheme: scheme,
-            pk: pk,
-        }
+        Verifier { scheme, pk }
     }
 }
 
